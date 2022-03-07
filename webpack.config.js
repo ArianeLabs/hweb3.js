be4160fe3ea6f22a961f6864c522be12f0b5c8c5
MicDeb-Ariane <michal.debek@arianelabs.com>
2022-03-07 14:25:42 +0100

    Add build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
     mode: "production",
     entry: {
-        web3: "./packages/hweb3/lib/index.js",
+        hweb3: "./packages/hweb3/lib/index.js",
     },
     plugins: [
         new CleanWebpackPlugin(),
